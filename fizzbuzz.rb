52e4bcee89ec3d474925e8379802ae22b77b9475
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-07-24 14:34:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
+def Fizzbuzz(num)
 (1..100).each do |num|
   if num % 15 == 0
     puts 'fizzbuzz'
