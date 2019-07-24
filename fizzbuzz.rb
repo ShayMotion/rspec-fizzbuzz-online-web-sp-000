eefaf7d4ae91eebfeb1f675f9073dd903e8138a4
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-07-24 13:46:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+range = (1..100)
+
+range.each do |n|
+  print "Fizz" if n % 3 == 0
+  print "Buzz" if n % 5 == 0
+  
+  puts "#{n}" if n % 3 ++ 0 || n % 5 == 0
+end
