8be4337b8755a3e1094121671c229a67be6a4d2b
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-07-24 14:40:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require_relative './fizzbuzz_spec.rb'
 
 def Fizzbuzz(num)
 (1..100).each do |num|
